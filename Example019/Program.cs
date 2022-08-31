625597f04483be76b64cc1c0fd92b36296834308
Евгений <ivanychcos@gmail.com>
2022-08-31 19:34:02 +0300

    Add example 019

---
+++ Example019/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите пятизначное число: ");
+int x = int.Parse(Console.ReadLine());
+
+if (x < 10000 || x > 99999){
+    Console.WriteLine("Введите пятизначное число!");
+}
+
+ else if(x % 10 == x / 10000 || x / 10 % 10 == x / 100 % 10){
+    Console.WriteLine("Да");
+}
+else {
+    Console.WriteLine("Нет");
+}
