67ed8874d7a93b9b7818a627328202269c9081c3
Евгений <ivanychcos@gmail.com>
2022-08-31 20:45:58 +0300

    Add example 023

---
+++ Example023/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Clear();
+Console.Write("Введите число: ");
+int x = int.Parse(Console.ReadLine());
+int index = 1;
+while(index <= x)
+{
+    Console.Write(Math.Pow(index, 3) + " ");
+    index++;
+}
+while(index >= x)
+{
+    Console.Write(Math.Pow(index, 3) + " ");
+    index--;
+}
