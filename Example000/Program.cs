961123609e9773dd5eaaaf253b3cb1f14924d192
Евгений <ivanychcos@gmail.com>
2022-09-01 12:48:50 +0300

    Add example 000

---
+++ Example000/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+Console.Write("Введите радиус: ");
+int n = int.Parse(Console.ReadLine());
+float p = 3.14F;
+double s = (Math.Pow(n, 2) * p);
+s = Math.Round(s, 0);
+Console.WriteLine(s);
+char [] array = s.ToString().ToCharArray();
+int index = 0;
+int indexMax = 0;
+while (index < array.Length){
+    if(array[index] > array[indexMax]){
+        indexMax = index;}
+        index++;
+    
+}
+Console.Write(array[indexMax]);
